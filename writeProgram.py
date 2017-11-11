d30d014ade78e7222da38ab8a8ca2447645a260c
nagatadaisuke <dbank0208@gmail.com>
2017-11-11 22:01:49 +0900

    add

---
+++ writeProgram.py
@@ -0,0 +1,20 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import os
+
+url = "http://dbank.sakura.ne.jp/iphoneX.html"
+response = urllib.request.urlopen(url)
+rss = response.read().decode("utf-8")
+soup = BeautifulSoup(rss, "xml")
+
+name = 0
+for s in soup.find_all("url"):
+    url = s.find_all("image_url")[0].string
+    name+=1
+    download_image(url,name)
+
+f = open('file.text', 'w')
+f.writelines(rss)
+f.close() 
+
+
